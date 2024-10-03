92767a0d69f303acc2e0eddc6155c9c8c6925a9d
medislodi <167592935+medislodi@users.noreply.github.com>
2024-10-03 03:51:57 -0400

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -38,31 +38,66 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
 
 function updateThumbnail(file) {
 	
-	document.getElementsByClassName("drop-zone")[0].style.display="none"
-
-	
 	const reader = new FileReader();
 	var filename;
 	var fileData;
-
-	reader.onload = function(e) {
-		const data = reader.result;
-		JSZip.loadAsync(data).then(function (zip) {
-			Object.keys(zip.files).forEach(function (filename) {
-				if (filename.includes(".json")) {
-					zip.files[filename].async('string').then(function (fileData) {
-						episode_json = JSON.parse(fileData);
-					})
+	var fixedname;
+	if (file.name.endsWith(".json")) {
+		reader.onload = (e) => {
+			var json_files = [];
+			episode_json = JSON.parse(e.target.result);
+			for (filename in episode_json["contents"]["resource"]) {
+				json_files.push(filename);
+			}
+			
+			document.querySelectorAll(".drop-zone__dir")[0].addEventListener("change",(f) => {
+				const filteredData = [...f.target.files].filter(row => json_files.includes(row.name));
+				for (const file of filteredData) {
+					file.arrayBuffer().then((fileData) => {
+						fixedname = 'resources/' + file.name;
+						dataresources[fixedname] = fileData;
+					});
 				}
-				else {
-					zip.files[filename].async('arraybuffer').then(function (fileData) {dataresources[filename] = fileData});
-			  }
-			})
-		})
-		setTimeout(()=>{
-			document.dispatchEvent(new CustomEvent('PhalconiaStart'));
-		},1000);
-	}
+			
+				setTimeout(()=>{
+					document.getElementsByClassName("drop-zone")[0].style.display="none"
+					document.dispatchEvent(new CustomEvent('PhalconiaStart'));
+				},1000);
+			},false);
+			
+			document.querySelectorAll(".drop-zone__dir").forEach((dirElement) => {
+				dirElement.click();
+			});
+			
+			
+		}
 
-	reader.readAsArrayBuffer(file);
+		reader.readAsText(file)
+	}
+	
+	else if (file.name.endsWith(".zip")) {
+
+		reader.onload = function(e) {
+			const data = reader.result;
+			JSZip.loadAsync(data).then(function (zip) {
+				Object.keys(zip.files).forEach(function (filename) {
+					if (filename.includes(".json")) {
+						zip.files[filename].async('string').then(function (fileData) {
+							episode_json = JSON.parse(fileData);
+						})
+					}
+					else {
+						zip.files[filename].async('arraybuffer').then(function (fileData) {dataresources[filename] = fileData});
+				  }
+				})
+			})
+			setTimeout(()=>{
+				document.getElementsByClassName("drop-zone")[0].style.display="none"
+				document.dispatchEvent(new CustomEvent('PhalconiaStart'));
+			},1000);
+		}
+
+		reader.readAsArrayBuffer(file);
+	
+	}
 }
