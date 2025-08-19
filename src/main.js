9de40c774f1e04f79c17d996717d4b9d6109b935
medislodi <167592935+medislodi@users.noreply.github.com>
2025-08-19 11:52:45 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -61,6 +61,7 @@ function updateThumbnail(file) {
 			
 				setTimeout(()=>{
 					document.getElementsByClassName("drop-zone")[0].style.display="none"
+					document.getElementById("app").style.display="block"
 					document.dispatchEvent(new CustomEvent('PhalconiaStart'));
 				},1000);
 			},false);
@@ -101,4 +102,5 @@ function updateThumbnail(file) {
 		reader.readAsArrayBuffer(file);
 	
 	}
+
 }
